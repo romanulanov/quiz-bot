55a244ec8565e8c15332a868f06549acc7529447
roman <turnsourcream@gmail.com>
2024-06-13 08:59:20 +0400

    fixed vk_bot

--- vk_bot.py
+++ vk_bot.py
@@ -11,6 +11,30 @@ from vk_api.longpoll import VkEventType, VkLongPoll
 from vk_api.utils import get_random_id
 from quiz import create_quiz_answers, create_quiz_questions, parse_question_file
 
+PHRASES = [
+    "Подыскиваю интересный вопрос. Минуточку...",
+    "Сейчас найду что-то интересное для обсуждения.",
+    "Хм, дай-ка подумать над вопросом для тебя.",
+    "Ищу что-то любопытное. Одну секунду...",
+    "Думаю над вопросом. Пожалуйста, подожди.",
+    "Хм, что бы тебе задать? Сейчас найду.",
+    "Ищу увлекательный вопрос. Момент...",
+    "Дай-ка подумаю... Найти бы хороший вопрос.",
+    "Сейчас придумаю что-то интересное. Подожди...",
+    "Хм, дай мне секунду, подумаю над вопросом.",
+    ]
+PHRASES_GIVE_UP = [
+    "Не сдавайся! Верь в себя!",
+    "Ты сможешь! Продолжай двигаться вперед!",
+    "Неважно, сколько времени уйдет, главное - не останавливайся!",
+    "Каждый шаг приближает тебя к цели!",
+    "Ты сильнее, чем думаешь! Продолжай бороться!",
+    "Помни, что важно - это не скорость, а упорство!",
+    "Самая трудная часть - это первый шаг. Ты уже сделал его!",
+    "Даже если путь к цели кажется трудным, помни, что каждый шаг приближает тебя к успеху!",
+    "Ты - настоящий боец! Продолжай идти вперед!",
+    "Ты можешь все, чего захочешь! Не останавливайся!"
+]
 
 def discussion_with_bot(event, vk_api, chat_data, quiz_questions, quiz_answers, r, user_id):
     keyboard = VkKeyboard(one_time=True)
@@ -38,7 +62,8 @@ def discussion_with_bot(event, vk_api, chat_data, quiz_questions, quiz_answers, 
 
 def handle_new_question_request(event, chat_data, quiz_questions, r, user_id):
     vk_question_id = random.choice(list(quiz_questions.keys()))
-    question_text = f'Вопрос {quiz_questions[vk_question_id]}'
+    phrase = random.choice(PHRASES)
+    question_text = f'{phrase}\n{quiz_questions[vk_question_id]}'
     r.set(event.user_id, question_text)
     return question_text
 
